caefd9d7095758c61d08c38758e00febcab6b525
sugiharaYutakaa <yutaka.sugihara@g.dreamcareer.co.jp>
2025-04-22 20:35:01 +0900

    fasdf

--- frontend/app/component/Home.tsx
+++ frontend/app/component/Home.tsx
@@ -1,44 +1,21 @@
+'use client';
 import Link from 'next/link';
+import { getUsers } from '@/app/api/usersApi';
+import { useState } from 'react';
 
 export default function Home() {
+    const [text, setText] = useState<string>("")
+    async function getTestData() {
+        const response = await getUsers()
+        setText(text + response.text)
+    }
     return (
         <>
             <div className="container mx-auto p-4">
-                <h1 className="text-3xl font-bold mb-6">スキル管理アプリケーション</h1>
-
-                <nav className="mb-8">
-                    <ul className="space-y-2">
-                        <li>
-                            <Link
-                                href="/user-skills"
-                                className="text-blue-500 hover:underline"
-                            >
-                                ユーザースキル一覧を表示
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href="/users"
-                                className="text-blue-500 hover:underline"
-                            >
-                                ユーザ一覧を表示
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href="/skills"
-                                className="text-blue-500 hover:underline"
-                            >
-                                スキル一覧を表示
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <div className="bg-gray-50 p-6 rounded">
-                    <h2 className="text-xl font-semibold mb-3">アプリケーションについて</h2>
-                    <p>このアプリケーションはユーザーのスキルを管理するためのツールです。</p>
-                </div>
+                <button className="bg-gray-300" onClick={getTestData}>PUSH</button>
+            </div>
+            <div className="container mx-auto p-4">
+                {text}
             </div>
         </>
     )
--- frontend/app/users/page.tsx
+++ frontend/app/users/page.tsx
@@ -1,56 +1,10 @@
 'use client';
-import { Users } from '@/app/types/Users';
-import { useState, useEffect } from 'react';
-import { getUsers } from '@/app/api/usersApi'
-import Table from '../component/Table';
-import { Column } from '../types/global';
+
+import Home from "../component/Home";
 
 export default function UserPage() {
-  const [users, setUsers] = useState<Users[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
 
-
-  useEffect(() => {
-    async function fetchSkills() {
-      try {
-        const data = await getUsers();
-        setUsers(data);
-      } catch (err) {
-        setError(err instanceof Error ? err.message : '予期せぬエラーが発生しました');
-      } finally {
-        setLoading(false);
-      }
-    }
-
-
-    fetchSkills();
-  }, []);
-
-  const columns: Column<Users>[] = [
-    { title: "ID", accessor: "id" },
-    { title: "名前", accessor: "name" },
-    { title: "カナ", accessor: "name_kana" },
-    { title: "email", accessor: "email" },
-    { title: "社員番号", accessor: "employee_number" },
-  ];
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">ユーザ一覧</h1>
-
-      {error && (
-        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
-          <p>{error}</p>
-        </div>
-      )}
-
-      {loading ? (
-        <p>読み込み中...</p>
-      ) : users.length === 0 ? (
-        <p>データがありません</p>
-      ) : (
-        <Table column={columns} columnDatas={users} />
-      )}
-    </div>
+    <Home />
   );
 }
